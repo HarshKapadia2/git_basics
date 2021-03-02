0b9e17921e611bd3c35ab1c8a1132218c96c9296
Harsh Kapadia <harshgkapadia@gmail.com>
2021-03-03 02:26:40 +0530

    :wrench: config: Add Google site verification file

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ echo "Building to: '${output}'";
 mkdir -p "${output}";
 
 for path in \
-	"static"            \
-	"manifest.json"     \
-	"service_worker.js" \
-	"robots.txt"        \
+	"static"                       \
+	"manifest.json"                \
+	"service_worker.js"            \
+	"robots.txt"                   \
+	"google66b7e1d4dbf56798.html"  \
 ; do
 	cp -r "src/${path}" "${output}/";
 done
