ddf1d7b4afa0cb84a520bb48b0ca7a024a2a0fc1
Harsh Kapadia <harshgkapadia@gmail.com>
2021-03-29 12:11:49 +0530

    :wrench: config: Add `.nojekyll` file to publish `.well-known` folder

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ for path in \
 	"service_worker.js"            \
 	"robots.txt"                   \
 	"google66b7e1d4dbf56798.html"  \
+	".nojekyll"                    \
 	".well-known"                  \
 	"privacy_policy.html"          \
 ; do
